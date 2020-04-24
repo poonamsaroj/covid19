a608d16e2b40bae330d9cd77ccb7f055043e71d2
poonamsaroj <sarojpoonam30@gmail.com>
2020-04-25 01:58:23 +0530

    Made heroku changes 1

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var cors = require('cors')
+
+let corsOptions = {
+    credentials: true,
+    origin: [process.env.ORIGIN_URL, 'http://localhost:8080'],
+    methods: ['GET', 'POST', 'PUT']
+}
 
 const app = express();
 
+app.use(cors(corsOptions));
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/covid19'));
 
